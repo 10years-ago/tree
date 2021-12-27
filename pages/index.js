c4eba2d47421b873d0b30ff0fc99ef8714043913
10years-ago <414578531@qq.com>
2021-12-27 18:21:16 +0800

    2021-12-27

--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,62 @@
-import Head from 'next/Head'
-import DecisionTree from 'decision-tree'
 import { useState } from 'react'
+const DecisionTree = require('decision-tree')
 // 訓練的數據
-const trainingData = [
-  { meals: '早餐', main: '麵包', drink: '牛奶', liked: true },
-  { meals: '早餐', main: '杯麵', drink: '可樂', liked: false },
-  { meals: '早餐', main: '雞蛋', drink: '牛奶', liked: false },
-  { meals: '午餐', main: '烏冬', drink: '可樂', liked: true },
-  { meals: '午餐', main: '牛扒', drink: '紅酒', liked: false },
-  { meals: '午餐', main: '豬扒', drink: '羅宋湯', liked: false },
-  { meals: '晚餐', main: '牛扒', drink: '紅酒', liked: false },
-  { meals: '晚餐', main: '豬扒', drink: '羅宋湯', liked: true },
-  { meals: '晚餐', main: '杯麵', drink: '可樂', liked: false }
-]
+// const trainingData = [
+//   { food: '燒鴨飯', drinks: '檸檬茶', liked: true },
+//   { food: '車仔麵', drinks: '奶茶', liked: false },
+//   { food: '叉燒飯', drinks: '鴛鴦', liked: true },
+//   { food: '烏冬面', drinks: '咖啡', liked: true },
+//   { food: '雞扒飯', drinks: '檸檬茶', liked: false },
+//   { food: '豬扒飯', drinks: '咖啡', liked: false },
+//   { food: '雞扒飯', drinks: '阿華田', liked: true },
+//   { food: '豬扒飯', drinks: '檸檬茶', liked: true },
+//   { food: '雞扒飯', drinks: '咖啡', liked: false },
+//   { food: '雞扒飯', drinks: '咖啡', liked: false },
+//   { food: '叉燒飯', drinks: '奶茶', liked: false }
+// ]
 
-const testData = [
-  { meals: '早餐', main: '麵包', drink: '牛奶', liked: true }
-]
+// const testData = [
+//   { food: '叉燒飯', drinks: '奶茶', liked: false }
+// ]
 
 export default function Home () {
-  // const [trainingData, setTrainingData] = useState([
-  // { meals: '早餐', drink: '麵包', drink: '牛奶', liked: true },
-  // { meals: '早餐', drink: '杯麵', drink: '可樂', liked: false },
-  // { meals: '早餐', drink: '雞蛋', drink: '牛奶', liked: false },
-  // { meals: '午餐', drink: '烏冬', drink: '可樂', liked: true },
-  // { meals: '午餐', drink: '牛扒', drink: '紅酒', liked: false },
-  // { meals: '午餐', drink: '豬扒', drink: '羅宋湯', liked: false },
-  // { meals: '晚餐', drink: '牛扒', drink: '紅酒', liked: true },
-  // { meals: '晚餐', drink: '豬扒', drink: '羅宋湯', liked: true },
-  // { meals: '晚餐', drink: '杯麵', drink: '可樂', liked: false }
-  // ])
+  const [trainingData, setTrainingData] = useState([
+    { food: '燒鴨飯', drinks: '檸檬茶', liked: true },
+    { food: '車仔麵', drinks: '奶茶', liked: false },
+    { food: '叉燒飯', drinks: '鴛鴦', liked: true },
+    { food: '烏冬面', drinks: '咖啡', liked: true },
+    { food: '雞扒飯', drinks: '檸檬茶', liked: false },
+    { food: '豬扒飯', drinks: '咖啡', liked: true },
+    { food: '豬扒飯', drinks: '咖啡', liked: true },
+    { food: '豬扒飯', drinks: '咖啡', liked: true },
+    { food: '豬扒飯', drinks: '咖啡', liked: false },
+    { food: '豬扒飯', drinks: '咖啡', liked: false },
+    { food: '豬扒飯', drinks: '咖啡', liked: false },
+    { food: '雞扒飯', drinks: '阿華田', liked: true },
+    { food: '豬扒飯', drinks: '檸檬茶', liked: true },
+    { food: '雞扒飯', drinks: '咖啡', liked: false },
+    { food: '雞扒飯', drinks: '咖啡', liked: false },
+    { food: '叉燒飯', drinks: '奶茶', liked: false }
+  ])
 
   // const [testData, setTestData] = useState([
-  //   { meals: '早餐', drink: '麵包', drink: '牛奶', liked: true }
+  //   { food: '豬扒飯', drinks: '咖啡', liked: true }
   // ])
 
   const [vail, setVail] = useState({
-    meals: '晚餐',
-    main: '豬扒',
-    drink: '羅宋湯'
+    food: '燒鴨飯',
+    drinks: '檸檬茶'
   })
 
-  const [temVail, setTemVail] = useState(['晚餐', '豬扒', '羅宋湯'])
+  const [addTrainingData, setAddTrainingData] = useState(['', '', 0])
+
+  const [temVail, setTemVail] = useState(['燒鴨飯', '檸檬茶'])
   // 判定結果的列
   const className = 'liked'
 
   // 用來預測的數據標籤
-  const features = ['meals', 'main', 'drink']
+  const features = ['food', 'drinks']
+  // const features = ['food', 'drinks']
 
   // 創建模型：
   const dt = new DecisionTree(trainingData, className, features)
@@ -54,14 +64,17 @@ export default function Home () {
   // console.log(dt)
   const predictedClass = dt.predict(vail)
   // 導入測試數據，返回樣本的準確性
-  const accuracy = dt.evaluate(testData)
+  const accuracy = dt.evaluate(trainingData)
   // const treeJson = dt.toJSON() // 底層模型，可用來可視化或者再次使用
   // const preTrainedDecisionTree = new DecisionTree(treeJson) 這樣可以直接使用這個訓練好的模型
   // dt.import(treeJson) 這樣可以直接在現有樹中導入這個訓練好的模型
-  console.log(predictedClass)
-  console.log(accuracy)
-  // console.log(dt)
-  // console.log(treeJson)
+
+  function deleted (arrIndex) {
+    setTrainingData(
+      trainingData.filter((item, index) => index !== arrIndex)
+    )
+  }
+
   return (
     <div>
       <h1 className='text-3xl font-bold pb-10'>
@@ -74,49 +87,54 @@ export default function Home () {
               這是訓練的數據
             </h1>
             <div className='flex'>
-              <span className='w-1/4'>時段</span>
               <span className='w-1/4'>主食</span>
               <span className='w-1/4'>飲料</span>
-              <span className='w-1/4'>是否喜歡</span>
+              <span className='w-1/4'>顧客評價</span>
+              <span className='w-1/4'>操作</span>
             </div>
             <div className='h-px bg-black my-2' />
             {
               trainingData.map((item, index) => {
                 return (
-                  <div key={index} className='flex'>
-                    <span className='w-1/4'>{item.meals}</span>
-                    <span className='w-1/4'>{item.main}</span>
-                    <span className='w-1/4'>{item.drink}</span>
+                  <div key={index} className='flex my-2'>
+                    <span className='w-1/4'>{item.food}</span>
+                    <span className='w-1/4'>{item.drinks}</span>
                     <span className='w-1/4'>{item.liked ? '喜歡' : '不喜歡'}</span>
+                    <div className='w-1/4'>
+                      <button
+                        className=' text-red-600'
+                        onClick={() => deleted(index)}
+                      >
+                          刪除數據
+                      </button>
+                    </div>
                   </div>
                 )
               })
             }
           </div>
-          <div className='w-96 text-center mt-10'>
+          {/* <div className='w-96 text-center mt-10'>
             <h1 className='text-xl font-bold'>
               這次測試數據，返回樣本的準確性
             </h1>
             <div className='flex'>
-              <span className='w-1/4'>時段</span>
-              <span className='w-1/4'>主食</span>
-              <span className='w-1/4'>飲料</span>
-              <span className='w-1/4'>是否喜歡</span>
+              <span className='w-1/3'>主食</span>
+              <span className='w-1/3'>飲料</span>
+              <span className='w-1/3'>顧客評價</span>
             </div>
             <div className='h-px bg-black my-2' />
             {
               testData.map((item, index) => {
                 return (
                   <div key={index} className='flex'>
-                    <span className='w-1/4'>{item.meals}</span>
-                    <span className='w-1/4'>{item.main}</span>
-                    <span className='w-1/4'>{item.drink}</span>
-                    <span className='w-1/4'>{item.liked ? '喜歡' : '不喜歡'}</span>
+                    <span className='w-1/3'>{item.food}</span>
+                    <span className='w-1/3'>{item.drinks}</span>
+                    <span className='w-1/3'>{item.liked ? '喜歡' : '不喜歡'}</span>
                   </div>
                 )
               })
             }
-          </div>
+          </div> */}
         </div>
         <div className=' ml-10'>
           <h3 className='text-xl font-bold pb-10'>
@@ -128,22 +146,23 @@ export default function Home () {
             </h3>
             <div className='w-96 text-center'>
               <div className='flex'>
-                <span className='w-1/4'>時段</span>
-                <span className='w-1/4'>主食</span>
-                <span className='w-1/4'>飲料</span>
-                <span className='w-1/4' />
+                <span className='w-1/3'>主食</span>
+                <span className='w-1/3'>飲料</span>
+                <span className='w-1/3' />
               </div>
               <div className='flex'>
-                <input className='w-1/4 px-1 border-black border-2 mx-1' value={temVail[0]} onChange={e => setTemVail([e.target.value, vail[1], vail[2]])} />
-                <input className='w-1/4 px-1 border-black border-2 mx-1' value={temVail[1]} onChange={e => setTemVail([vail[0], e.target.value, vail[2]])} />
-                <input className='w-1/4 px-1 border-black border-2 mx-1' value={temVail[2]} onChange={e => setTemVail([vail[0], vail[1], e.target.value])} />
+                <input
+                  className='w-1/3 px-1 border-black border-2 mx-1'
+                  value={temVail[0]}
+                  onChange={e => setTemVail([e.target.value, temVail[1]])}
+                />
+                <input className='w-1/3 px-1 border-black border-2 mx-1' value={temVail[1]} onChange={e => setTemVail([temVail[0], e.target.value])} />
                 <button
-                  className='w-1/4 bg-blue-700 hover:bg-blue-600 text-white border-gray-400'
+                  className='w-1/3 bg-blue-700 hover:bg-blue-600 text-white border-gray-400'
                   onClick={() => {
                     setVail({
-                      meals: temVail[0],
-                      main: temVail[1],
-                      drink: temVail[2]
+                      food: temVail[0],
+                      drinks: temVail[1]
                     })
                   }}
                 >
@@ -153,6 +172,56 @@ export default function Home () {
               <h3 className='text-xl font-bold pb-5'>
                 驗證結果：{predictedClass ? '喜歡' : '不喜歡'}
               </h3>
+            </div>
+          </div>
+          <div>
+            <h3 className='text-xl font-bold pb-5'>
+              添加樣本：顧客評價(0：不喜歡，1：喜歡)
+            </h3>
+            <div className='w-96 text-center'>
+              <div className='flex'>
+                <span className='w-1/4'>主食</span>
+                <span className='w-1/4'>飲料</span>
+                <span className='w-1/4'>顧客評價</span>
+                <span className='w-1/4'>操作</span>
+              </div>
+              <div className='flex'>
+                <input
+                  className='w-1/4 px-1 border-black border-2 mx-1'
+                  value={addTrainingData[0]}
+                  onChange={e => setAddTrainingData([e.target.value, addTrainingData[1], addTrainingData[2]])}
+                />
+                <input
+                  className='w-1/4 px-1 border-black border-2 mx-1'
+                  value={addTrainingData[1]}
+                  onChange={e => setAddTrainingData([addTrainingData[0], e.target.value, addTrainingData[2]])}
+                />
+                <input
+                  className='w-1/4 px-1 border-black border-2 mx-1'
+                  value={addTrainingData[2]}
+                  onChange={e => setAddTrainingData([addTrainingData[0], addTrainingData[1], e.target.value])}
+                />
+                <button
+                  className='w-1/4 bg-blue-700 hover:bg-blue-600 text-white border-gray-400'
+                  onClick={() => {
+                    console.log(addTrainingData[2])
+                    if (addTrainingData[2] !== 0 && addTrainingData[2] !== 1) {
+                      window.alert('顧客評價內只能填入0或者1')
+                    } else {
+                      setTrainingData(
+                        [
+                          ...trainingData,
+                          {
+                            food: addTrainingData[0], drinks: addTrainingData[1], liked: addTrainingData[3] === 1
+                          }
+                        ]
+                      )
+                    }
+                  }}
+                >
+                    添加
+                </button>
+              </div>
             </div>
           </div>
         </div>

b231225fbbbd3811058be218235027f14c52a261
10years-ago <414578531@qq.com>
2021-12-28 13:14:07 +0800

    添加csv導入

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import CSVReader from 'react-csv-reader'
 const DecisionTree = require('../lib/decision-tree')
 // 訓練的數據
 // const trainingData = [
@@ -19,44 +20,54 @@ const DecisionTree = require('../lib/decision-tree')
 //   { food: '叉燒飯', drinks: '奶茶', liked: false }
 // ]
 
+// const food = ['燒鴨飯', '公仔麵', '叉燒飯', '烏冬面', '咖喱雞飯', '番茄豬扒飯', '蛋炒飯', '乾炒牛河', '煎雙蛋', '番茄牛肉通心粉', '柱侯牛腩飯', '咖喱牛腩飯', '沙爹牛肉飯', '洋蔥豬扒飯']
+
+// const drinks = ['咖啡', '黑咖啡', '港式奶茶', '鴛鴦', '黑白鴛鴦', '好立克', '阿華田', '檸茶', '可樂', '檸七', '檸樂', '橙汁']
+// const drinksLength = drinks.length
+// const newDrinks = []
+
+// for (let i = 0; i < 5000; i++) {
+//   newDrinks.push(drinks[Math.floor(Math.random() * drinksLength)])
+// }
+
 export default function Home () {
-  const [trainingData, setTrainingData] = useState([
-    { food: '燒鴨飯', drinks: '檸檬茶', liked: true },
-    { food: '車仔麵', drinks: '奶茶', liked: false },
-    { food: '叉燒飯', drinks: '鴛鴦', liked: true },
-    { food: '烏冬面', drinks: '咖啡', liked: true },
-    { food: '雞扒飯', drinks: '檸檬茶', liked: false },
-    { food: '豬扒飯', drinks: '咖啡', liked: true },
-    { food: '豬扒飯', drinks: '咖啡', liked: true },
-    { food: '豬扒飯', drinks: '咖啡', liked: true },
-    { food: '豬扒飯', drinks: '咖啡', liked: false },
-    { food: '豬扒飯', drinks: '咖啡', liked: false },
-    { food: '豬扒飯', drinks: '咖啡', liked: false },
-    { food: '雞扒飯', drinks: '阿華田', liked: true },
-    { food: '豬扒飯', drinks: '檸檬茶', liked: true },
-    { food: '雞扒飯', drinks: '咖啡', liked: false },
-    { food: '雞扒飯', drinks: '咖啡', liked: false },
-    { food: '叉燒飯', drinks: '奶茶', liked: false }
-  ])
+  const [trainingData, setTrainingData] = useState([{}])
+  // const [trainingData, setTrainingData] = useState([
+  //   { food: '燒鴨飯', drinks: '檸檬茶', liked: true },
+  //   { food: '車仔麵', drinks: '奶茶', liked: false },
+  //   { food: '叉燒飯', drinks: '鴛鴦', liked: true },
+  //   { food: '烏冬面', drinks: '咖啡', liked: true },
+  //   { food: '雞扒飯', drinks: '檸檬茶', liked: false },
+  //   { food: '豬扒飯', drinks: '咖啡', liked: true },
+  //   { food: '豬扒飯', drinks: '咖啡', liked: true },
+  //   { food: '豬扒飯', drinks: '咖啡', liked: true },
+  //   { food: '豬扒飯', drinks: '咖啡', liked: false },
+  //   { food: '豬扒飯', drinks: '咖啡', liked: false },
+  //   { food: '豬扒飯', drinks: '咖啡', liked: false },
+  //   { food: '雞扒飯', drinks: '阿華田', liked: true },
+  //   { food: '豬扒飯', drinks: '檸檬茶', liked: true },
+  //   { food: '雞扒飯', drinks: '咖啡', liked: false },
+  //   { food: '雞扒飯', drinks: '咖啡', liked: false },
+  //   { food: '叉燒飯', drinks: '奶茶', liked: false }
+  // ])
 
   // const [testData, setTestData] = useState([
   //   { food: '豬扒飯', drinks: '咖啡', liked: true }
   // ])
 
   const [vail, setVail] = useState({
-    food: '燒鴨飯',
-    drinks: '檸檬茶'
+    food: '',
+    drinks: ''
   })
 
   const [addTrainingData, setAddTrainingData] = useState(['', '', 0])
 
-  const [temVail, setTemVail] = useState(['燒鴨飯', '檸檬茶'])
-  // 判定結果的列
-  const className = 'liked'
-
+  const [temVail, setTemVail] = useState(['', ''])
   // 用來預測的數據標籤
-  const features = ['food', 'drinks']
-  // const features = ['food', 'drinks']
+  const [features, setFeatures] = useState(['food', 'drinks'])
+  const [className, setClassName] = useState('liked')
+  // 判定結果的列
+  // const className = 'liked'
 
   // 創建模型：
   const dt = new DecisionTree(trainingData, className, features)
@@ -94,7 +105,7 @@ export default function Home () {
             </div>
             <div className='h-px bg-black my-2' />
             {
-              trainingData.map((item, index) => {
+              !!trainingData[0].food && trainingData.map((item, index) => {
                 return (
                   <div key={index} className='flex my-2'>
                     <span className='w-1/4'>{item.food}</span>
@@ -111,6 +122,11 @@ export default function Home () {
                   </div>
                 )
               })
+            }
+            {
+              !trainingData[0].food && (
+                <div>無數據</div>
+              )
             }
           </div>
           {/* <div className='w-96 text-center mt-10'>
@@ -137,8 +153,26 @@ export default function Home () {
           </div> */}
         </div>
         <div className=' ml-10'>
+          <CSVReader
+            onFileLoaded={(data, fileInfo, originalFile) => {
+              // console.log(data)
+              setFeatures([data[0][0], data[0][1]])
+              setClassName(data[0][2])
+              data.shift()
+              setTrainingData(data.map(item => {
+                return { food: item[0], drinks: item[1], liked: item[2] === '1' }
+              }
+              ))
+            }}
+          />
           <h3 className='text-xl font-bold pb-10'>
-            樣本準確性:   {accuracy * 100}%
+            樣本準確性: {accuracy * 100}%
+          </h3>
+          <h3 className='text-xl font-bold pb-10'>
+            樣本標籤列: {features[0]}、{features[1]}
+          </h3>
+          <h3 className='text-xl font-bold pb-10'>
+            樣本預測結果列: {className}
           </h3>
           <div>
             <h3 className='text-xl font-bold pb-5'>

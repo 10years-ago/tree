1d08da65b5a63eca87fd44bc9e5fad7bde687e42
10years-ago <414578531@qq.com>
2021-12-28 19:10:18 +0800

    修改訓練的數據

--- pages/index.js
+++ pages/index.js
@@ -32,6 +32,7 @@ const DecisionTree = require('../lib/decision-tree')
 
 export default function Home () {
   const [trainingData, setTrainingData] = useState([{}])
+  // console.log(trainingData)
   // const [trainingData, setTrainingData] = useState([
   //   { food: '燒鴨飯', drinks: '檸檬茶', liked: true },
   //   { food: '車仔麵', drinks: '奶茶', liked: false },
@@ -56,16 +57,19 @@ export default function Home () {
   // ])
 
   const [vail, setVail] = useState({
-    food: '',
-    drinks: ''
+    building_num: '',
+    has_old_people: '',
+    has_child: '',
+    floor_num: '',
+    is_usual_residence: ''
   })
 
-  const [addTrainingData, setAddTrainingData] = useState(['', '', 0])
+  // const [addTrainingData, setAddTrainingData] = useState(['', '', '', '', '', 0])
 
-  const [temVail, setTemVail] = useState(['', ''])
+  const [temVail, setTemVail] = useState(['', '', '', '', ''])
   // 用來預測的數據標籤
-  const [features, setFeatures] = useState(['food', 'drinks'])
-  const [className, setClassName] = useState('liked')
+  const [features, setFeatures] = useState(['building_num', 'has_old_people', 'has_child', 'floor_num', 'is_usual_residence'])
+  const [className, setClassName] = useState('voted')
   // 判定結果的列
   // const className = 'liked'
 
@@ -85,7 +89,9 @@ export default function Home () {
       trainingData.filter((item, index) => index !== arrIndex)
     )
   }
-
+  console.log(dt)
+  console.log(vail)
+  console.log(predictedClass)
   return (
     <div>
       <h1 className='text-3xl font-bold pb-10'>
@@ -93,25 +99,31 @@ export default function Home () {
       </h1>
       <div className='flex'>
         <div>
-          <div className='w-96 text-center'>
+          <div className='text-center' style={{ width: '800px' }}>
             <h1 className='text-xl font-bold'>
               這是訓練的數據
             </h1>
-            <div className='flex'>
-              <span className='w-1/4'>主食</span>
-              <span className='w-1/4'>飲料</span>
-              <span className='w-1/4'>顧客評價</span>
-              <span className='w-1/4'>操作</span>
+            <div className='flex w-full'>
+              <span className=' w-20 mr-2'>棟別</span>
+              <span className='w-20 mr-2'>是否有老人</span>
+              <span className='w-20 mr-2'>是否有小孩</span>
+              <span className='w-20 mr-2'>樓層</span>
+              <span className='w-20 mr-2'>是否常住</span>
+              <span className='w-20 mr-2'>是否讚成</span>
+              <span className='w-20 mr-2'>操作</span>
             </div>
             <div className='h-px bg-black my-2' />
             {
-              !!trainingData[0].food && trainingData.map((item, index) => {
+              !!trainingData[0].building_num && trainingData.map((item, index) => {
                 return (
-                  <div key={index} className='flex my-2'>
-                    <span className='w-1/4'>{item.food}</span>
-                    <span className='w-1/4'>{item.drinks}</span>
-                    <span className='w-1/4'>{item.liked ? '喜歡' : '不喜歡'}</span>
-                    <div className='w-1/4'>
+                  <div key={index} className='flex w-full my-2 text-center'>
+                    <span className='w-20 mr-2'>{item.building_num}</span>
+                    <span className='w-20 mr-2'>{item.has_old_people === '1' ? '是' : '否'}</span>
+                    <span className='w-20 mr-2'>{item.has_child === '1' ? '是' : '否'}</span>
+                    <span className='w-20 mr-2'>{item.floor_num}</span>
+                    <span className='w-20 mr-2'>{item.is_usual_residence === '1' ? '是' : '否'}</span>
+                    <span className='w-20 mr-2'>{item.voted ? '讚成' : '不讚成'}</span>
+                    <div className='w-20 mr-2'>
                       <button
                         className=' text-red-600'
                         onClick={() => deleted(index)}
@@ -124,12 +136,12 @@ export default function Home () {
               })
             }
             {
-              !trainingData[0].food && (
+              !trainingData[0].building_num && (
                 <div>無數據</div>
               )
             }
           </div>
-          {/* <div className='w-96 text-center mt-10'>
+          {/* <div className='w-1/3 text-center mt-10'>
             <h1 className='text-xl font-bold'>
               這次測試數據，返回樣本的準確性
             </h1>
@@ -145,7 +157,7 @@ export default function Home () {
                   <div key={index} className='flex'>
                     <span className='w-1/3'>{item.food}</span>
                     <span className='w-1/3'>{item.drinks}</span>
-                    <span className='w-1/3'>{item.liked ? '喜歡' : '不喜歡'}</span>
+                    <span className='w-1/3'>{item.liked ? '讚成' : '不讚成'}</span>
                   </div>
                 )
               })
@@ -156,11 +168,20 @@ export default function Home () {
           <CSVReader
             onFileLoaded={(data, fileInfo, originalFile) => {
               // console.log(data)
-              setFeatures([data[0][0], data[0][1]])
-              setClassName(data[0][2])
+              setFeatures([data[0][0], data[0][1], data[0][2], data[0][3], data[0][4]])
+              setClassName(data[0][5])
               data.shift()
+              data.pop()
+              // data.unshift()
               setTrainingData(data.map(item => {
-                return { food: item[0], drinks: item[1], liked: item[2] === '1' }
+                return {
+                  building_num: item[0],
+                  has_old_people: item[1],
+                  has_child: item[2],
+                  floor_num: item[3],
+                  is_usual_residence: item[4],
+                  voted: item[5] === '1'
+                }
               }
               ))
             }}
@@ -169,7 +190,10 @@ export default function Home () {
             樣本準確性: {accuracy * 100}%
           </h3>
           <h3 className='text-xl font-bold pb-10'>
-            樣本標籤列: {features[0]}、{features[1]}
+            樣本標籤列:
+            {
+              features.map((item, index) => { return <span key={index}>{item}、</span> })
+            }
           </h3>
           <h3 className='text-xl font-bold pb-10'>
             樣本預測結果列: {className}
@@ -178,25 +202,33 @@ export default function Home () {
             <h3 className='text-xl font-bold pb-5'>
               驗證：
             </h3>
-            <div className='w-96 text-center'>
+            <div className='w-auto text-center'>
               <div className='flex'>
-                <span className='w-1/3'>主食</span>
-                <span className='w-1/3'>飲料</span>
-                <span className='w-1/3' />
+                <span className='w-1/6'>棟別</span>
+                <span className='w-1/6'>是否有老人</span>
+                <span className='w-1/6'>是否有小孩</span>
+                <span className='w-1/6'>樓層</span>
+                <span className='w-1/6'>是否常住</span>
               </div>
               <div className='flex'>
                 <input
-                  className='w-1/3 px-1 border-black border-2 mx-1'
+                  className='w-1/6 px-1 border-black border-2 mx-1'
                   value={temVail[0]}
-                  onChange={e => setTemVail([e.target.value, temVail[1]])}
+                  onChange={e => setTemVail([e.target.value, temVail[1], temVail[2], temVail[3], temVail[4]])}
                 />
-                <input className='w-1/3 px-1 border-black border-2 mx-1' value={temVail[1]} onChange={e => setTemVail([temVail[0], e.target.value])} />
+                <input className='w-1/6 px-1 border-black border-2 mx-1' value={temVail[1]} onChange={e => setTemVail([temVail[0], e.target.value, temVail[2], temVail[3], temVail[4]])} />
+                <input className='w-1/6 px-1 border-black border-2 mx-1' value={temVail[2]} onChange={e => setTemVail([temVail[0], temVail[1], e.target.value, temVail[3], temVail[4]])} />
+                <input className='w-1/6 px-1 border-black border-2 mx-1' value={temVail[3]} onChange={e => setTemVail([temVail[0], temVail[1], temVail[2], e.target.value, temVail[4]])} />
+                <input className='w-1/6 px-1 border-black border-2 mx-1' value={temVail[4]} onChange={e => setTemVail([temVail[0], temVail[1], temVail[2], temVail[3], e.target.value])} />
                 <button
-                  className='w-1/3 bg-blue-700 hover:bg-blue-600 text-white border-gray-400'
+                  className='w-20 bg-blue-700 hover:bg-blue-600 text-white border-gray-400'
                   onClick={() => {
                     setVail({
-                      food: temVail[0],
-                      drinks: temVail[1]
+                      building_num: String(temVail[0]),
+                      has_old_people: String(temVail[1]),
+                      has_child: String(temVail[2]),
+                      floor_num: String(temVail[3]),
+                      is_usual_residence: String(temVail[4])
                     })
                   }}
                 >
@@ -204,39 +236,39 @@ export default function Home () {
                 </button>
               </div>
               <h3 className='text-xl font-bold pb-5'>
-                驗證結果：{predictedClass ? '喜歡' : '不喜歡'}
+                驗證結果：{predictedClass ? '讚成' : '不讚成'}
               </h3>
             </div>
           </div>
-          <div>
+          {/* <div>
             <h3 className='text-xl font-bold pb-5'>
               添加樣本：顧客評價(0：不喜歡，1：喜歡)
             </h3>
-            <div className='w-96 text-center'>
+            <div className='w-1/3 text-center'>
               <div className='flex'>
-                <span className='w-1/4'>主食</span>
-                <span className='w-1/4'>飲料</span>
-                <span className='w-1/4'>顧客評價</span>
-                <span className='w-1/4'>操作</span>
+                <span className='w-20'>主食</span>
+                <span className='w-20'>飲料</span>
+                <span className='w-20'>顧客評價</span>
+                <span className='w-20'>操作</span>
               </div>
               <div className='flex'>
                 <input
-                  className='w-1/4 px-1 border-black border-2 mx-1'
+                  className='w-20 px-1 border-black border-2 mx-1'
                   value={addTrainingData[0]}
                   onChange={e => setAddTrainingData([e.target.value, addTrainingData[1], addTrainingData[2]])}
                 />
                 <input
-                  className='w-1/4 px-1 border-black border-2 mx-1'
+                  className='w-20 px-1 border-black border-2 mx-1'
                   value={addTrainingData[1]}
                   onChange={e => setAddTrainingData([addTrainingData[0], e.target.value, addTrainingData[2]])}
                 />
                 <input
-                  className='w-1/4 px-1 border-black border-2 mx-1'
+                  className='w-20 px-1 border-black border-2 mx-1'
                   value={addTrainingData[2]}
                   onChange={e => setAddTrainingData([addTrainingData[0], addTrainingData[1], Number(e.target.value)])}
                 />
                 <button
-                  className='w-1/4 bg-blue-700 hover:bg-blue-600 text-white border-gray-400'
+                  className='w-20 bg-blue-700 hover:bg-blue-600 text-white border-gray-400'
                   onClick={() => {
                     console.log(addTrainingData[2])
                     if (addTrainingData[2] !== 0 && addTrainingData[2] !== 1) {
@@ -257,7 +289,7 @@ export default function Home () {
                 </button>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
